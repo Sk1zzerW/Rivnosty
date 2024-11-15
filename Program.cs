8c97d133857614d3cafad5d679caca0515b7be3a
olegk <kbryzhenyukpc@gmail.com>
2024-11-15 23:08:37 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 3, 5, 8, 2, 10, 7, 1, 6 };
+
+        Console.WriteLine("ваше число");
+        int userInput = int.Parse(Console.ReadLine());
+
+        int count = 0;
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] < userInput)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine("Кол-во элементов меньше" + userInput + ": " + count);
+
+    }
+}
